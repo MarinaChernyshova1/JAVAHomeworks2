a962685b4023c8c2dcd0e8928db8ec23fc04028a
MarinaCh <marina3123273@mail.ru>
2023-04-04 11:11:09 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 13676;
+        int x = 20;
+        int y = ticketPrice/x;
+
+
+
+        System.out.println("Итого миль " + y);
+
+    }
+}
